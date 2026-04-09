2436f5410544f0d3002d72844a057d5859459fa3
DiptiLecturer <lecturer3.amad.nsda@dipti.com.bd>
2026-04-09 12:46:06 +0600

    "auth with repo without validation done "

--- app/src/main/java/org/freedu/simplelocationshareb7/AuthActivity.kt
+++ app/src/main/java/org/freedu/simplelocationshareb7/AuthActivity.kt
@@ -1,17 +1,71 @@
 package org.freedu.simplelocationshareb7
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import org.freedu.simplelocationshareb7.Repo.UserRepository
 import org.freedu.simplelocationshareb7.databinding.ActivityAuthBinding
+import org.freedu.simplelocationshareb7.viewModels.AuthViewModel
 
 class AuthActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAuthBinding
 
+    private val repo = UserRepository()
+
+    private val viewModel: AuthViewModel by viewModels<AuthViewModel> {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return AuthViewModel(repo) as T
+            }
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+
+            val email = binding.email.text.toString().trim()
+            val password = binding.password.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                return@setOnClickListener
+            }
+            viewModel.login(email, password)
+
+        }
+        binding.btnRegister.setOnClickListener {
+
+            val email = binding.email.text.toString().trim()
+            val password = binding.password.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                return@setOnClickListener
+            }
+
+            viewModel.register(email, password)
+        }
+        viewModel.registerResult.observe(this) { (success, message) ->
+            if (success) {
+                Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Registration Filed", Toast.LENGTH_SHORT).show()
+            }
+        }
+        viewModel.loginResult.observe(this) { (success, message) ->
+            if (success) {
+                Toast.makeText(this, "Logged In Successful", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Logged In Filed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
 
     }
 }
--- app/src/main/java/org/freedu/simplelocationshareb7/viewModels/AuthViewModel.kt
+++ app/src/main/java/org/freedu/simplelocationshareb7/viewModels/AuthViewModel.kt
@@ -6,5 +6,22 @@ import org.freedu.simplelocationshareb7.Repo.UserRepository
 
 class AuthViewModel(private val repo: UserRepository) : ViewModel() {
 
+    val loginResult = MutableLiveData<Pair<Boolean, String?>>()
+    val registerResult = MutableLiveData<Pair<Boolean, String?>>()
+
+
+    fun login(email: String, password: String) {
+
+        repo.loginUser(email, password) { success, message ->
+          loginResult.postValue(success to message)
+        }
+    }
+
+    fun register(email: String, password: String) {
+        repo.registerUser(email, password) { success, message ->
+          registerResult.postValue(success to message)
+        }
+
+    }
 
 }
